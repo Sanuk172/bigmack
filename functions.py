b22c4e861adcb777785f627ddf60b2afd38f6d33
vlasov <vaasovsasa1w3@gmail.com>
2024-02-01 18:55:51 +0500

    third

---
+++ functions.py
@@ -0,0 +1,61 @@
+import requests
+
+
+def get_static(**params):
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=params)
+    return response.content
+
+
+def geocoder(**params):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    response = requests.get(geocoder_api_server, params=params)
+
+    if not response:
+        print('Oh no Error', response.status_code, response.reason)
+        exit(-1)
+    return response.json()
+
+
+def get_toponym(json_data):
+    return json_data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+
+
+def get_toponym_coord(toponym):
+    return list(map(float, toponym["Point"]["pos"].split(' ')))
+
+
+def get_spn(toponym):
+    lon1, lat1 = map(float, toponym['boundedBy']['Envelope']['lowerCorner'].split())
+    lon2, lat2 = map(float, toponym['boundedBy']['Envelope']['upperCorner'].split())
+    return lon2 - lon1, lat2 - lat1
+
+
+def search_organisation(**params):
+    search_api_server = "https://search-maps.yandex.ru/v1/"
+    api_key = 'dda3ddba-c9ea-4ead-9010-f43fbc15c6e3'
+    params['apikey'] = api_key
+    response = requests.get(search_api_server, params=params)
+
+    if not response:
+        print('Oh no Error', response.status_code, response.reason)
+        exit(-1)
+
+    return response.json()
+
+
+def get_organisation(json_data):
+    organization = json_data["features"]
+    return organization
+
+
+def get_org_info(organization):
+    org_name = organization["properties"]["CompanyMetaData"]["name"]
+    org_address = organization["properties"]["CompanyMetaData"]["address"]
+    org_hours = organization["properties"]["CompanyMetaData"]["Hours"]['text']
+    return org_name, org_address, org_hours
+
+
+def get_org_coord(organization):
+    point = organization['geometry']['coordinates']
+    return point
---
+++ main.py
@@ -0,0 +1,53 @@
+from io import BytesIO
+
+import pygame
+from functions import get_static
+
+
+class BigMap:
+    def __init__(self):
+        self.image = None
+        self.lon, self.lat = 60.153191, 55.156353
+        self.layer = 'map'
+        self.z = 0
+        self.update_map()
+
+    def update_map(self):
+        map_params = {
+            "l": "map",
+            "z": self.z,
+            "ll": ",".join(map(str, (self.lon, self.lat))),
+            "size": "650,450"
+        }
+        image = BytesIO(get_static(**map_params))
+        self.image = pygame.image.load(image)
+
+    def draw(self, surf):
+        surf.blit(self.image, (0, 0))
+
+    def event_handler(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                self.z = min(self.z + 1, 21)
+            if event.key == pygame.K_PAGEDOWN:
+                self.z = max(self.z - 1, 0)
+            if event.key == pygame.K_LEFT:
+                self.lon = (self.lon - 180 - 300) % 180 + 180
+            if event.key == pygame.K_RIGHT:
+                self.lon = (self.lon - 180 + 300) % 180 + 180
+            self.update_map()
+
+
+pygame.init()
+SIZE = W, H = 650, 450
+screen = pygame.display.set_mode(SIZE)
+app = BigMap()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        app.event_handler(event)
+    screen.fill(pygame.Color('black'))
+    app.draw(screen)
+    pygame.display.flip()
