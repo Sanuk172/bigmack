993ac35f0b48d689e12f29bacb3ca75df927eba9
vlasov <vaasovsasa1w3@gmail.com>
2024-02-01 19:14:44 +0500

    fourth comit

--- main.py
+++ main.py
@@ -1,43 +1,29 @@
-import os
-import sys
 from io import BytesIO
+
 import pygame
-from funcs import get_static
-
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
+from functions import get_static
 
 
 class BigMap:
     def __init__(self):
         self.image = None
-        self.ll = 60.153191, 55.156353
+        self.lon, self.lat = 60.153191, 55.156353
         self.layer = 'map'
-        self.z = 17
+        self.z = 0
         self.update_map()
 
     def update_map(self):
         map_params = {
-            "ll": ",".join(map(str, self.ll)),
-            'z': self.z,
             "l": "map",
-            'size': '650,450'
+            "z": self.z,
+            "ll": ",".join(map(str, (self.lon, self.lat))),
+            "size": "650,450"
         }
         image = BytesIO(get_static(**map_params))
         self.image = pygame.image.load(image)
+
+    def draw(self, surf):
+        surf.blit(self.image, (0, 0))
 
     def event_handler(self, event):
         if event.type == pygame.KEYDOWN:
@@ -45,25 +31,27 @@ class BigMap:
                 self.z = min(self.z + 1, 21)
             if event.key == pygame.K_PAGEDOWN:
                 self.z = max(self.z - 1, 0)
+            if event.key == pygame.K_LEFT:
+                self.lon = (self.lon - 180 - 200 * 2 ** (-self.z)) % 360 - 180
+            if event.key == pygame.K_RIGHT:
+                self.lon = (self.lon - 180 + 200 * 2 ** (-self.z)) % 360 - 180
+            if event.key == pygame.K_UP:
+                self.lat = min(self.lat + 70 * 2 ** (-self.z), 88)
+            if event.key == pygame.K_DOWN:
+                self.lat = min(self.lat - 70 * 2 ** (-self.z), -88)
             self.update_map()
 
-    def draw(self, surf):
-        surf.blit(self.image, (0, 0))
 
-
-if __name__ == '__main__':
-    pygame.init()
-    SIZE = W, H = 650, 450
-    screen = pygame.display.set_mode(SIZE)
-    app = BigMap()
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            app.event_handler(event)
-
-        screen.fill('black')
-        app.draw(screen)
-        pygame.display.flip()
+pygame.init()
+SIZE = W, H = 650, 450
+screen = pygame.display.set_mode(SIZE)
+app = BigMap()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        app.event_handler(event)
+    screen.fill(pygame.Color('black'))
+    app.draw(screen)
+    pygame.display.flip()
